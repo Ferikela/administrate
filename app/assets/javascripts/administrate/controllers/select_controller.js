f116bf983a04c7c5a20bbe8e1914aecbc52b4714
Olivier de Robert <olivier.derobert@gmail.com>
2024-11-25 15:18:46 +0100

    Update select_controller.js remove console.log

--- app/assets/javascripts/administrate/controllers/select_controller.js
+++ app/assets/javascripts/administrate/controllers/select_controller.js
@@ -3,7 +3,6 @@ import $ from "jquery";
 
 export default class extends Controller {
   connect() {
-    console.log("Hello there!", this.element);
     $(this.element).selectize({
       allowEmptyOption: true
     });
